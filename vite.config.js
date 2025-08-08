3f20475022f87e7ef43694048d6b35a2e5ad0d2b
Sei <test@test.co.jp>
2025-08-08 16:56:23 +0900

    adding public/build

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    base: '/build/',  // 追加（public/build がルートなのでここを指定）
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
